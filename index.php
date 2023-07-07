457f5ac8fddbf5860c563de32d045fe879b44bc8
Vanessa Felix Christoff <vfchristoff9@gmail.com>
2023-07-07 04:12:18 -0300

    Change repo

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
-
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
   //Variables
   $textStr = "";
   $textDef = "";
@@ -21,14 +22,14 @@
   if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
   }
-  
+
   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $textStr = $_POST['textStr'];
-    $textDef = $_POST['textDef'];
-    $textMag = $_POST['textMag'];
-    $textSpi = $_POST['textSpi'];
-    $textSpd = $_POST['textSpd'];
-    $textLuk = $_POST['textLuk'];
+    $textStr = isset($_POST['textStr']) ? $_POST['textStr'] : '';
+    $textDef = isset($_POST['textDef']) ? $_POST['textDef'] : '';
+    $textMag = isset($_POST['textMag']) ? $_POST['textMag'] : '';
+    $textSpi = isset($_POST['textSpi']) ? $_POST['textSpi'] : '';
+    $textSpd = isset($_POST['textSpd']) ? $_POST['textSpd'] : '';
+    $textLuk = isset($_POST['textLuk']) ? $_POST['textLuk'] : '';
 
     echo "Data received successfully!";
 
